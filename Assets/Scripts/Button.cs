344e938520758397d15c94948131efb9aef4e75c
Wes Rockholz <wesley.rockholz@gmail.com>
2014-12-06 17:28:42 -0500

    Fixed Buttons, Sprites
    - Fixed buttons to work without 4.6 GUI patch.
    - Fixed low-res large sprites.

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public Sprite idleSprite;
+	public Sprite hoverSprite;
+
+	private SpriteRenderer sr;
+
+	// Use this for initialization
+	protected virtual void Start () {
+		sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// Fires when the button is clicked. Override for effect.
+	/// </summary>
+	protected virtual void OnClick() {
+	}
+
+	/// <summary>
+	/// Raises the mouse enter event.
+	/// </summary>
+	void OnMouseEnter() {
+		sr.sprite = hoverSprite;
+	}
+
+	/// <summary>
+	/// Raises the mouse exit event.
+	/// </summary>
+	void OnMouseExit() {
+		sr.sprite = idleSprite;
+	}
+
+	/// <summary>
+	/// Raises the mouse up event.
+	/// </summary>
+	void OnMouseUp() {
+		OnClick ();
+	}
+}
---
+++ Assets/Scripts/PlayAgainButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAgainButton : Button {
+
+	private GameScreen screen;
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start ();
+
+		screen = transform.parent.GetComponent<GameScreen>();
+	}
+
+	/// <summary>
+	/// Fires when the button is clicked.
+	/// </summary>
+	protected override void OnClick ()
+	{
+		base.OnClick ();
+
+		screen.LoadOtherScreen(0);
+	}
+}
